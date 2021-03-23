e749b8a108603c9c1c6e2f70b343ad6c34e3f908
oriolnadalvilar <79997489+oriolnadalvilar@users.noreply.github.com>
2021-03-23 12:26:48 +0100

    Add files via upload

---
+++ uytransfer/header.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="estilos.css">
+
+<table id="header">
+	<tr>
+		<td id="titleHeader">Uy!Transfer</td>
+		<td id="subHeader">Enviar archivo | Mis últimos archivos</td>
+	</tr>
+</table>
